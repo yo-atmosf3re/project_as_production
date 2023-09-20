8cf806d84d417a66ff12ab1e764c50bdb79bf10d
Alexandr Shishov <ernesto.chrome@mail.ru>
2023-09-20 13:15:12 +0300

    fix(build): исправление плагинов webpack - то, что не нужно в проде больше туда не попадает;

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -17,15 +17,14 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
-
     ];
 
     if (isDev) {
         // plugins.push(new ReactRefreshWebpackPlugin())
         plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
     }
     return plugins;
 }
