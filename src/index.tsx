c9ca8f1391f245cca2c90a8d1a67f9dc1993f86f
Alexandr Shishov <ernesto.chrome@mail.ru>
2023-10-13 23:22:52 +0300

    fix: test commit;

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import { Suspense } from 'react';
 import { render } from 'react-dom';
 import { StoreProvider } from 'app/providers/StoreProvider';
 import { BrowserRouter } from 'react-router-dom';
-import { ErrorBoundary } from 'app/providers/ErrorBoundary';
+import {
+    ErrorBoundary,
+} from 'app/providers/ErrorBoundary';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import App from './app/App';
 import 'app/styles/index.scss';
