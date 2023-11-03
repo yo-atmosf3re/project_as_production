b1a8629b7848c2bf691168117260dcb513908c15
Alexandr Shishov <ernesto.chrome@mail.ru>
2023-11-03 16:37:27 +0300

    fix: исправление теста;

--- src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.test.ts
+++ src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.test.ts
@@ -1,7 +1,6 @@
 import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
 import { initArticlesPage } from './initArticlesPage';
 import { fetchArticlesList } from '../fetchArticlesList/fetchArticlesList';
-import { getArticlesPageInited } from '../../selectors/getArticlesPageInited/getArticlesPageInited';
 
 jest.mock('../fetchArticlesList/fetchArticlesList');
 
@@ -39,41 +38,4 @@ describe('initArticlesPage', () => {
         expect(thunk.dispatch).toBeCalledTimes(2);
         expect(fetchArticlesList).not.toHaveBeenCalled();
     });
-
-    // test('Function fetchArticlesList has been not called, because hasMore it\'s false', async () => {
-    //     const thunk = new TestAsyncThunk(fetchNextArticlesPage, {
-    //         articlesPage: {
-    //             page: 2,
-    //             ids: [],
-    //             entities: {},
-    //             limit: 5,
-    //             isLoading: false,
-    //             hasMore: false,
-    //         },
-    //     });
-    //     await thunk.callThunk();
-
-    //     // ? Функция dispatch вызывается всего лишь два раза, потому что здесь не срабатывает условие из fetchNextArticlesPage, внутри которого, при успешном hasMore и неуспешном isLoading, вызываются остальные две функции dispatch;
-    //     expect(thunk.dispatch).toBeCalledTimes(2);
-    //     // ? По причинам, которые описаны выше, функция fetchArticlesList не вызывается;
-    //     expect(fetchArticlesList).not.toHaveBeenCalled();
-    // });
-
-    // test('Function fetchArticlesList has been not called, because isLoading it\'s true', async () => {
-    //     const thunk = new TestAsyncThunk(fetchNextArticlesPage, {
-    //         articlesPage: {
-    //             page: 2,
-    //             ids: [],
-    //             entities: {},
-    //             limit: 5,
-    //             isLoading: true,
-    //             hasMore: true,
-    //         },
-    //     });
-    //     await thunk.callThunk();
-
-    //     expect(thunk.dispatch).toBeCalledTimes(2);
-    //     // ? Во время загрузки функция fetchArticlesList не может быть вызвана;
-    //     expect(fetchArticlesList).not.toHaveBeenCalled();
-    // });
 });
