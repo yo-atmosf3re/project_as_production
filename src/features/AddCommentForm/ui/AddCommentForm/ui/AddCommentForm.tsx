193ee1759929ec781567185fa460bd974d616e66
Alexandr Shishov <ernesto.chrome@mail.ru>
2024-01-25 17:51:58 +0300

    feat(eslint): обновлена версия самописного модуля с правилами для Eslint, добавлены новые настройки: теперь Eslint на проекте может работать с алиасами, потому что для этого был улучшен самописный плагин;

--- src/features/AddCommentForm/ui/AddCommentForm/ui/AddCommentForm.tsx
+++ src/features/AddCommentForm/ui/AddCommentForm/ui/AddCommentForm.tsx
@@ -1,10 +1,10 @@
 import React, { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { Input } from '@/shared/ui/Input';
 import { Button } from '@/shared/ui/Button';
 import { DynamicModuleLoader, ReducersList } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { useSelector } from 'react-redux';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { HStack } from '@/shared/ui/Stack';
 import { getCommentFormText } from '../../../model/selectors/getCommentFormText/getCommentFormText';
