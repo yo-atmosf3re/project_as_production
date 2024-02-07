8cd2c6dd475b425449851196043fc4d77ebfcec6
Alexandr Shishov <ernesto.chrome@mail.ru>
2024-02-07 19:39:52 +0300

    feat(scripts): добавление комментариев, исправлений;

--- scripts/clearCache/clearCache.js
+++ scripts/clearCache/clearCache.js
@@ -2,27 +2,33 @@
 var path = require('path');
 var fs = require('fs');
 
+// ? Получение пути до папки, которую нужно удалить;
 var cachePathFolder = process.cwd() + '\\node_modules\\.cache';
-console.log(cachePathFolder, 'cachePathFolder');
 
 (
     function clearCache(folderPath) {
+    // ? Флаг, показывающий существует ли директория по указанному пути;
     var folderExist = fs.existsSync(cachePathFolder)
         
+        // ? Проверка существования директории на основании вышеописанного флага;
         if(folderExist) {
+            // ? Рекурсивное удаление;
             fs.readdirSync(folderPath).forEach((file, index) => {
+                // ? Текущий файл;
                 var currentPath = path.join(folderPath, file);
                 
-                if (
-                    fs.lstatSync(currentPath).isDirectory()
-                ) {
-                clearCache(currentPath);
+                // ? Проверка на директорию текущего файла;
+                if (fs.lstatSync(currentPath).isDirectory()) {
+                    // ? Вызов функции до тех пор, пока передаваемый текущий файл не перестанет являться директорией;
+                    clearCache(currentPath);
                 } else {
-                fs.unlinkSync(currentPath);
+                    // ? Удаление файла в случае, если передаваемый текущий файл не является директорией;
+                    fs.unlinkSync(currentPath);
                 }
                 
             });
             
+            // ? Удаление пустой директории;
             fs.rmdirSync(folderPath);
             console.log('CACHE HAS BEEN CLEARED');
         }
