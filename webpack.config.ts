a2c7578190c4fc6abd47cd2c46992f8ea6a1daaf
Alexandr Shishov <ernesto.chrome@mail.ru>
2024-03-14 10:41:37 +0300

    feat(build): добавлена функция, которая при различных модах (дев или прод), возвращает различный URL для доступа к бекенду;

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,6 +3,18 @@ import path from 'path';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
 import { BuildEnv, BuildMode, BuildPaths } from './config/build/types/config';
 
+function getApiUrl(mode: BuildMode, apiUrl?: string) {
+    if(apiUrl) {
+        return apiUrl;
+    }
+
+    if(mode === 'production') {
+        return '/api';
+    }
+
+    return 'http://localhost:7777'
+}
+
 export default (env: BuildEnv) => {
     const paths: BuildPaths = {
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
@@ -15,7 +27,7 @@ export default (env: BuildEnv) => {
 
     const mode: BuildMode = env?.mode || 'development';
     const PORT: number = env?.port || 8888;
-    const apiUrl = env?.apiUrl || 'http://localhost:7777';
+    const apiUrl = getApiUrl(mode, env?.apiUrl);
 
     const isDev: boolean = mode === 'development';
 
