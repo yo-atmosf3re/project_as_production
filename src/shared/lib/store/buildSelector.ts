0613e549db885a1052e1321ae419570ee67b6f87
Alexandr Shishov <ernesto.chrome@mail.ru>
2024-03-14 15:26:45 +0300

    feat(rtk): улучшен buildSelector: добавлена специальная типизация и функционал расширяющий возможности buildSelector - теперь есть возможность передавать аргументы в вызываемую функцию, которую создаёт и возвращает buildSelector;

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -1,17 +1,19 @@
 import { useSelector } from 'react-redux';
 import { StateSchema } from '@/app/providers/StoreProvider';
 
-type SelectorType<T> = (state: StateSchema) => T;
+type SelectorType<T, Args extends any[]> = (state: StateSchema, ...args: Args) => T;
 
-type ResultType<T> = [() => T, SelectorType<T>];
+type Hook<T, Args extends any[]> = (...args: Args) => T;
+
+type ResultType<T, Args extends any[]> = [Hook<T, Args>, SelectorType<T, Args>];
 
 /**
  * Функция, которая оборачивает принимаемый аргумент `selector` в хук useSelector, а затем возвращает кортеж, где первый элемент - `useSelectorHook` - функция, которая возвращает обёрнутый в useSelector `selector`, а второй аргумент - сам `selector`;
  * @param selector
  */
-export function buildSelector<T>(selector: SelectorType<T>): ResultType<T> {
-    const useSelectorHook = () => {
-        return useSelector(selector);
+export function buildSelector<T, Args extends any[]>(selector: SelectorType<T, Args>): ResultType<T, Args> {
+    const useSelectorHook: Hook<T, Args> = (...args: Args) => {
+        return useSelector((state: StateSchema ) => selector(state, ...args));
     };
 
     return [useSelectorHook, selector];
