b24dce155a8a34d80c4435ca1d59f5963b8092b7
Alexandr Shishov <ernesto.chrome@mail.ru>
2024-04-22 18:09:34 +0300

    fix;

--- src/app/lib/useAppToolbar.tsx
+++ src/app/lib/useAppToolbar.tsx
@@ -4,7 +4,7 @@ import { ScrollToolbar } from '@/widgets/ScrollToolbar';
 import { useRouteChange } from '@/shared/lib/router/useRouteChange';
 
 /**
- * Функция, которая в зависимости от страницы, отрисовывает `ScrollToolbar`;
+ * Функция, которая в зависимости от страницы, отрисовывает `ScrollToolbar` или любую другую компоненту;
  */
 export function useAppToolbar() {
     const appRoute = useRouteChange();
@@ -12,8 +12,6 @@ export function useAppToolbar() {
     const toolbarByAppRoute: OptionalRecord<APP_ROUTES, ReactElement> = {
         [APP_ROUTES.ARTICLES]: <ScrollToolbar />,
         [APP_ROUTES.ARTICLE_DETAILS]: <ScrollToolbar />,
-        [APP_ROUTES.MAIN]: <div>123</div>,
-        [APP_ROUTES.ABOUT]: <div>123</div>,
     };
 
     return toolbarByAppRoute[appRoute];
